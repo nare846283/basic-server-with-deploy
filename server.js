dad6d8aa1ab52138952aed0cba7252d0473091bd
narendra ahirwar <narendraahirwar37@gmail.com>
2025-01-18 14:09:56 +0530

    c

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
+// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// Default route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
